1149d591822336a30f5b3bcf91419441faf27b8b
Andrés Ramírez Quirós <andrew.rq@gmail.com>
2019-06-16 00:50:51 -0600

    Documentación y optimización de ORManager

--- src/main/java/com/digipet/prototype/auxiliar/ORManager.java
+++ src/main/java/com/digipet/prototype/auxiliar/ORManager.java
@@ -10,24 +10,44 @@ import javax.persistence.criteria.CriteriaBuilder;
 import javax.persistence.criteria.CriteriaQuery;
 import java.util.List;
 
+/**
+ * Controlador que maneja las comuicaciones de los ClassEntity con la base de datos
+ * @param <T> tipo del objeto a comunicar
+ */
 public class ORManager<T> {
 
-    private final Class<T> typeParameterClass;
-    private final T object;
+    private final Class<T> typeParameterClass; //Clase del objeto
+    private final T object; //Objeto a comunicar
+    private final SessionFactory sessionFactory; //SessionFactory necesario para la comunicación
+    private final Session session; //Session necesario para la comunicación
 
+    /**
+     * Constructor del ORManager
+     * @param typeParameterClass clase del objeto
+     * @param object objeto a comunicar
+     */
     public ORManager(Class<T> typeParameterClass, T object) {
         this.typeParameterClass = typeParameterClass;
         this.object = object;
-    }
-
-
-    public void crearObjeto(){
         try {
             Configuration configuration = new Configuration().configure();
             ServiceRegistry serviceRegistry = new StandardServiceRegistryBuilder().applySettings(configuration.getProperties()).build();
-            SessionFactory sessionFactory = configuration.buildSessionFactory(serviceRegistry);
-            Session session = sessionFactory.openSession();
+            sessionFactory = configuration.buildSessionFactory(serviceRegistry);
+            session = sessionFactory.openSession();
+        } catch (Exception e){
+            System.out.println("Error al crear servicio en el constructor");
+            throw e;
+        }
+    }
 
+    /**
+     * Método para crear un objeto en la base de datos. El objeto a guardar es el instanciado en el constructor
+     */
+    public void crearObjeto() throws IllegalArgumentException{
+
+        if (this.object == null) throw new IllegalArgumentException("Intento de crear un objeto cuando su valor es nulo.");
+
+        try {
             session.beginTransaction();
             session.save(this.object);
             session.getTransaction().commit();
@@ -35,25 +55,21 @@ public class ORManager<T> {
             session.close();
             sessionFactory.close();
 
-        } catch (org.hibernate.MappingException exception){
-            System.out.println("Tipo de clase inválido: debe ser un objeto de clase \'Entity\'");
-            throw exception;
         } catch (Exception exception){
             System.out.println("Error no identificado");
             throw exception;
         }
     }
 
-    public List<T> obtenerTabla(String queryString){
+    /**
+     * Método que retorna una lista con todas las instancias de la clase
+     * @return lista de objetos
+     */
+    public List<T> obtenerTabla(){
 
         List<T> data; //Navigate the object using an iterator
 
         try {
-            Configuration configuration = new Configuration().configure();
-            ServiceRegistry serviceRegistry = new StandardServiceRegistryBuilder().applySettings(configuration.getProperties()).build();
-            SessionFactory sessionFactory = configuration.buildSessionFactory(serviceRegistry);
-            Session session = sessionFactory.openSession();
-
             CriteriaBuilder builder = session.getCriteriaBuilder();
             CriteriaQuery<T> criteria = builder.createQuery(typeParameterClass);
             criteria.from(typeParameterClass);
@@ -62,9 +78,6 @@ public class ORManager<T> {
             session.close();
             sessionFactory.close();
 
-        } catch (org.hibernate.MappingException exception){
-            System.out.println("Tipo de clase inválido: debe ser un objeto de clase \'Entity\'");
-            throw exception;
         } catch (Exception exception){
             System.out.println("Error no identificado");
             throw exception;
@@ -73,25 +86,22 @@ public class ORManager<T> {
         return data;
     }
 
+    /**
+     * Método que retorna el objeto según el id, de la tabla de la clase instanciada
+     * @param identificador id del objeto a buscar en la base de datos
+     * @return objeto encontrado //TODO: manejar retorno de objeto nulo
+     */
     public T obtenerObjeto(int identificador){
 
         T data;
 
         try {
-            Configuration configuration = new Configuration().configure();
-            ServiceRegistry serviceRegistry = new StandardServiceRegistryBuilder().applySettings(configuration.getProperties()).build();
-            SessionFactory sessionFactory = configuration.buildSessionFactory(serviceRegistry);
-            Session session = sessionFactory.openSession();
-
             session.beginTransaction();
-            data = session.get(this.typeParameterClass, identificador);
+            data = session.get(this.typeParameterClass, identificador); //TODO: excepción identificador inválido
 
             session.close();
             sessionFactory.close();
 
-        } catch (org.hibernate.MappingException exception){
-            System.out.println("Tipo de clase inválido: debe ser un objeto de clase \'Entity\'");
-            throw exception;
         } catch (Exception exception){
             System.out.println("Error no identificado");
             throw exception;
