61531a20e297b762715c3f01938b778ec43a86db
Andrés Ramírez Quirós <andrew.rq@gmail.com>
2019-06-16 16:23:07 -0600

    Auto stash before merge of "hibernate" and "origin/master"

--- src/main/test/com/digipet/prototype/auxiliar/ORManagerTest.java
+++ src/main/test/com/digipet/prototype/auxiliar/ORManagerTest.java
@@ -5,6 +5,7 @@ import com.digipet.prototype.orm.BadgeEntity;
 import com.digipet.prototype.orm.MascotaEntity;
 import com.digipet.prototype.orm.UsuarioEntity;
 
+import java.sql.Date;
 import java.util.List;
 
 import static org.junit.Assert.assertEquals;
@@ -28,5 +29,14 @@ public class ORManagerTest {
 
     @Test
     public void ejecutarSelect() {
+    }
+
+    @Test
+    public void registrarClienteSP() {
+        String datestr = "2015-03-31";
+        Date date = Date.valueOf(datestr);
+        com.digipet.prototype.auxiliar.ORManager.registrarClienteSP("Gutierrez","Casimiro,",
+                "Alfaro","Alajuela","Alajuela","no@correo","","87655432",
+                "nohay.jpg","laprogre","Salu2",date);
     }
 }
